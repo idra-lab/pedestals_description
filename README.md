7758b51b42eda273ad61f99c2f41db2bd23f5f28
elamon <lamonedoardo@gmail.com>
2025-02-20 21:47:56 +0100

    Update README and launch file.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # pedestals_description
 ROS2 description of robot pedestals
+
+## Overview
+
+The `pedestals_description` package provides a URDF description for the pedestals of the IDRA lab. Pedestals avialable:
+- **kuka_pedestal**: Pedestal for the KUKA iiwa 14, to be used with [this repo](https://github.com/idra-lab/lbr_fri_ros2_stack/tree/pedestal) (pedestal branch);
+- **doosan_pedestal**: Pedestal for the Doosan H2515 (to be added);
+
+For each pedestal
+
+The pedestal xacro defines two frames:
+- **pedestal_base_frame:** Represents the base of the pedestal at ground level.
+- **pedestal_top_frame:** Serves as the attachment point for the robot base.
+
+A ROS 2 launch file processes these xacro files, merges their URDF outputs into a single robot description, and visualizes the model in RViz2.
+
+## Usage (standalone)
+To launch the pedestal description and view the model in `RViz2`, run:
+
+```bash
+ros2 launch pedestals_description display.launch.py pedestal_model:=kuka_pedestal
+```
+
+![Pedestal](asset/rviz.png)
--- launch/display.launch.py
+++ launch/display.launch.py
@@ -4,22 +4,51 @@ import os, xacro
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+# from launch.substitutions import LaunchConfiguration, PythonExpression
+from launch.substitutions import (
+    Command,
+    FindExecutable,
+    LaunchConfiguration,
+    PathJoinSubstitution,
+)
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
-    # Path to your URDF file
-    xacro_file = os.path.join(
-        get_package_share_directory('pedestals_description'),
-        'urdf',
-        'kuka_pedestal.xacro'
+    
+    pedestal_model_arg = DeclareLaunchArgument(
+        'pedestal_model',
+        default_value='kuka_pedestal',
+        description='Type of pedestal to display',
+        choices=["kuka_pedestal", "doosan_pedestal"]
     )
+    
+    # Path to your URDF file
+    # xacro_file = os.path.join(
+    #     get_package_share_directory('pedestals_description'),
+    #     'urdf',
+    #     pedestal_model
+    # )
+    
+    # robot_description_content = xacro.process_file(xacro_file).toxml()
+    
+    robot_description_content = Command([
+        FindExecutable(name="xacro"),
+        " ",
+        PathJoinSubstitution(
+            [
+                FindPackageShare("pedestals_description"),
+                "urdf",
+                LaunchConfiguration('pedestal_model'),
+            ]
+        ),
+        ".xacro"])
     
     rviz_config_file = os.path.join(
         get_package_share_directory('pedestals_description'),
         'rviz',
         'config.rviz'
     )
-    
-    robot_description_content = xacro.process_file(xacro_file).toxml()
     
     rviz_node = Node(
         package='rviz2',
@@ -39,6 +68,7 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
+        pedestal_model_arg,
         rviz_node,
         robot_state_publisher_node
     ])
